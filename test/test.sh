f9973b53e5018f7a0049fb3ab185d035703075c3
oleh <olehmisar@gmail.com>
2026-03-03 01:13:53 +0400

    simple test

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# check if the version of aztec is the same as the version in the .aztecrc file
-
-gaztec_version=$(source ../gaztec --version)
-file_version=$(cat .aztecrc)
-
-if [[ "$gaztec_version" != "$file_version" ]]; then
-  echo "Test failed: gaztec version $gaztec_version is not the same as the version in the .aztecrc file $file_version"
-  exit 1
-fi
+source ../gaztec --version
 
 echo "Test passed"
